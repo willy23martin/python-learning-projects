51ee4775fba4fa4a7735eb70bcc478a90cdb42d4
William Martín Chávez González <ingmartic@gmail.com>
2020-05-03 16:17:20 -0500

    Funzioni e procedure 1

---
+++ 1-Training/eduopen_learning/variabili_ed_uso_della_memoria/Spunti-di-approfondimento-Funzioni e procedure.py
@@ -0,0 +1,13 @@
+"""
+Santiago de Cali, Maggio 3 di 2020
+Studente: William Martín Chávez González
+A) Utilizzare il comando dir(_builtins_) per avere un
+elenco delle funzioni e procedure predefinite in Python.
+B) Provare ad utilizzare alcune funzioni predefinite all´interno
+di espressioni aritmetiche.
+"""
+
+dir(__builtins__)
+print("parola".__contains__("o"))
+print((5**2).__eq__(25))
+print("altezza".__len__())
