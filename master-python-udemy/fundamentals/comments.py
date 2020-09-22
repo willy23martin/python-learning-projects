d0f187ee78d2467d6eac9045ec3f31e480ab9b5e
wchavez <wchavez@gns.com.co>
2020-09-22 18:56:50 -0500

    Types and Variables

---
+++ master-python-udemy/fundamentals/comments.py
@@ -0,0 +1,9 @@
+# This is the greeting Hello World by William Chávez.
+print("Hello")
+print("World")
+print("from WMC")
+
+"""
+This is a multiline comment.
+print("Multiline comments").
+"""
---
+++ master-python-udemy/fundamentals/variables/variables.py
@@ -0,0 +1,19 @@
+title = "Python applied to BI Analytics in Business Pocesses and RPA"
+print(title)
+number = 109
+print(number)
+text_2="Python does not defined let syntax"
+print(text_2)
+decimal = 48.23
+print(decimal)
+
+# String concatenation:
+text_1 = "To be concatenated with"
+text_2 = "the modification of the text_2"
+print(text_1 + " " + text_2)
+
+#String interpolation:
+print(f"{text_1} {text_2}")
+print("This is an example of method format with text_1: {} {}".format(text_1, text_2))
+# Isolated example
+print(text_1, text_2)
