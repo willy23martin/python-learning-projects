3adeffc05cc985766592a9d5bc7dcd60c833a1dc
willy23martin <william.chavez@correo.icesi.edu.co>
2020-11-22 10:01:59 -0500

    Casting values from a data type to another

---
+++ master-python-udemy/fundamentals/variables/casting_variables.py
@@ -0,0 +1,10 @@
+"""
+Transform a value from a data_type to another
+"""
+
+number_a = 90
+print(type(number_a))
+print("The next s str casting: " + str(number_a))
+print(type(number_a))
+print("The next s float casting: ", float(str(number_a)))
+print("The next s int casting: ",  int(float(str(number_a))))
