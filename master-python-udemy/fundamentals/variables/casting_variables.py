bedb2f876d68cdbc391421f35c7aefc9a48bbb18
willy23martin <william.chavez@correo.icesi.edu.co>
2020-11-22 10:06:04 -0500

    String curiosities

--- master-python-udemy/fundamentals/variables/casting_variables.py
+++ master-python-udemy/fundamentals/variables/casting_variables.py
@@ -8,3 +8,4 @@ print("The next s str casting: " + str(number_a))
 print(type(number_a))
 print("The next s float casting: ", float(str(number_a)))
 print("The next s int casting: ",  int(float(str(number_a))))
+
---
+++ master-python-udemy/fundamentals/variables/string_proprerties.py
@@ -0,0 +1,6 @@
+"""
+Some curiosities
+"""
+str_a = "'Hi'"
+str_b = '"There"'
+print(str_a, " \n ", str_b)
