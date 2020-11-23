20a19d9b4af044641e56934f689b1ddb0a056f8b
willy23martin <william.chavez@correo.icesi.edu.co>
2020-11-23 12:59:26 -0500

    Flask web applications fundamentals

--- openweb-training/flask/app.py
+++ openweb-training/flask/app.py
@@ -1,11 +1,11 @@
 from flask import Flask # Constructor
 app = Flask(__name__) # Application
 
-@app.route('/')
-def microservices_api():
+@app.route('/') # Decortator - main route of the application
+def microservices_api(): # Without parameters
     return {
         "status": "200 OK",
         "msg":"Welcome to Microservices API in Flask with Python"
-    }
+    } # HTTP response object
 if __name__ == '__main__':
-    app.run()
+    app.run('0.0.0.0', 5000, debug=True) # Run web server, accept requests from any source by the port 5000 and run it in debug mode
