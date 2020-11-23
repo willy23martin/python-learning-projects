66a606050603e979484bdcc39215927fd2ee9d84
willy23martin <william.chavez@correo.icesi.edu.co>
2020-11-23 13:04:59 -0500

    Flask web applications fundamentals - PIN Debbuger

--- openweb-training/flask/app.py
+++ openweb-training/flask/app.py
@@ -3,9 +3,11 @@ app = Flask(__name__) # Application
 
 @app.route('/') # Decortator - main route of the application
 def microservices_api(): # Without parameters
+    a = 'Basic Flask web application'
+    b = int(a) # For debugging using the PIN
     return {
         "status": "200 OK",
         "msg":"Welcome to Microservices API in Flask with Python"
-    } # HTTP response object
+    } # HTTP response JSON Object
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000, debug=True) # Run web server, accept requests from any source by the port 5000 and run it in debug mode
