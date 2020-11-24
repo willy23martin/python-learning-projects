3f6fee47248c9a1d5001c767227aa73eda146335
willy23martin <william.chavez@correo.icesi.edu.co>
2020-11-23 20:25:19 -0500

    Python-Flask simple web API - Contacts Endpoint was added with /contacts/<int:id> to search a contact

--- openweb-training/flask/app_fundamentals/app.py
+++ openweb-training/flask/app_fundamentals/app.py
@@ -1,6 +1,21 @@
 from flask import Flask # Constructor
 app = Flask(__name__) # Application
 
+contacts = [
+            {
+                "contact": {
+                    "id": 1123231,
+                    "name": "Name A"
+                }
+            },
+            {
+                "contact": {
+                    "id": 1125261,
+                    "name": "Name B"
+                }
+            }
+        ]
+
 @app.route('/') # Decortator - main route of the application
 def microservices_api(): # Without parameters
     # Only for debugging purposes
@@ -17,15 +32,23 @@ def microservices_api(): # Without parameters
 @app.route('/contacts/') # Between /.../ in order to grant the navigation to this endpoint
 def contacts_api():
     return {
-        "contacts": [
-            {
-                "contact": 1123231
-            },
-            {
-                "contact": 1125262
-            }
-        ]
+        "contacts": contacts
     }
+
+@app.route('/contacts/<int:id>')
+def contacts_api_contact_endpoint(id):
+    response = {
+        "status": '404 Not Found',
+        "msg": f"There is no contact with id: {id} in our database"
+    }
+    for index in range(0, len(contacts)):
+        if contacts[index]["contact"]["id"] == id:
+            response = {
+                "status": "200 OK",
+                "msg": contacts[index]
+            }
+            break
+    return response
 
 # Remove if you use manager.py
 if __name__ == '__main__':
